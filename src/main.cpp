e4c22e48c1dca484b3cde895c549002834282503
asvavilov <vavilov.rf@gmail.com>
2021-02-26 23:42:29 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+// TODO решить с платой: с wifi или оставить nano и добавить физическое управление
+
+#define redPin 11
+#define greenPin 10
+#define bluePin 9
+
+#define DELAY 10
+
+int val;
+
+void setup() {
+	pinMode(redPin, OUTPUT);
+	pinMode(bluePin, OUTPUT);
+	pinMode(greenPin, OUTPUT);
+}
+
+void loop()
+{
+	for (val = 255; val > 0; val--)
+	{
+		analogWrite(redPin, val);  //set PWM value for red
+		analogWrite(greenPin, 128 - val); //set PWM value for green
+		analogWrite(bluePin, 255 - val); //set PWM value for blue
+
+		delay(DELAY);
+	}
+	for (val = 0; val < 255; val++)
+	{
+		analogWrite(redPin, val);
+		analogWrite(greenPin, 128 - val);
+		analogWrite(bluePin, 255 - val);
+
+		delay(DELAY);
+	}
+}
